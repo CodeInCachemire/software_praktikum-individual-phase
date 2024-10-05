63f406eb69359fd09cc7ae0c937a79b07977f9b9
ahmad qureshi <ahmadqmfi2@gmail.com>
2024-10-05 11:52:28 +0200

    Scenario COnstants are fixed should pass all Json

--- src/main/kotlin/de/unisaarland/cs/se/selab/Constants.kt
+++ src/main/kotlin/de/unisaarland/cs/se/selab/Constants.kt
@@ -18,6 +18,6 @@ object Constants {
     const val MAX_DRIFT_INTENSITY = 10
     const val MIN_OIL_AMOUNT = 0
     const val MAX_OIL_AMOUNT = 1000
-    const val MIN_STRENGTH = 4
+    const val MIN_STRENGTH = 1
     const val MAX_STRENGTH = 4
 }
